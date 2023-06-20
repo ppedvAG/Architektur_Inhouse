d2388a1f536226eb5cf1dbba6d633a8207ba2aee
André <AndreR@ppedv.de>
2023-06-20 16:25:06 +0200

    BankAccount impl

--- Calculator/Calculator/BankAccount.cs
+++ Calculator/Calculator/BankAccount.cs
@@ -2,16 +2,26 @@
 {
     public class BankAccount
     {
-        public decimal Balance { get; }
+        public decimal Balance { get; private set; }
 
         public void Deposit(decimal v)
         {
-            throw new NotImplementedException();
+            if (v <= 0)
+                throw new ArgumentException();
+
+            Balance += v;
         }
 
         public void Withdraw(decimal v)
         {
-            throw new NotImplementedException();
+            if (v <= 0)
+                throw new ArgumentException();
+
+            if (v > Balance)
+                throw new InvalidOperationException();
+
+            Balance -= v;
+
         }
     }
 }
