943793be387867380648282eeeb54d75a8281223
André <AndreR@ppedv.de>
2023-06-21 14:14:04 +0200

    EfCore ist da

---
+++ ppedv.TastyToGo/ppedv.TastyToGo.Data.Db/TastyToGoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.TastyToGo.Model;
+
+namespace ppedv.TastyToGo.Data.Db
+{
+    public class TastyToGoContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        string conString;
+
+        public TastyToGoContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+    }
+}
